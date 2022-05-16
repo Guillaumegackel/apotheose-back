687ebabe2fbe104f35ec32ffaeef34c28c9d9dd6
Webme <guillaume.gackel@gmail.com>
2022-05-16 15:23:31 +0200

    bdd local connection

---
+++ data/bdd1.sql
@@ -0,0 +1,62 @@
+-- Deploy c_du_props:init to pg
+
+BEGIN;
+
+CREATE TABLE "user" (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  pseudonym TEXT NOT NULL,
+  avatar_img TEXT,
+  created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE "home" ( 
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  password INT UNIQUE DEFAULT FLOOR (RANDOM()*(9999)),
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  user_id INT NOT NULL REFERENCES "user"(id) UNIQUE
+);
+
+CREATE TABLE "generic_task" (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  value INT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+
+CREATE TABLE "home_task" (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  value INT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  home_id INT NOT NULL REFERENCES "home"(id) 
+);
+
+CREATE TABLE "done_task" (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  value INT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  home_id INT NOT NULL REFERENCES "home"(id) , 
+  user_id INT NOT NULL REFERENCES "user"(id) 
+);
+
+CREATE TABLE "reward" (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT DEFAULT NULL,
+  description TEXT DEFAULT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  home_id INT REFERENCES "home"(id) NOT NULL UNIQUE
+);
+
+CREATE TABLE "attributed_task" (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id INT REFERENCES "user"(id),
+  home_task_id INT REFERENCES "home_task"(id),
+  created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+COMMIT;
